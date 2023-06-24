a284748241b50c024c78f31f73f83e29df981286
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-06-24 20:40:56 +0200

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: :author_id, dependent: :destroy
+  has_many :likes, foreign_key: :author_id, dependent: :destroy
+  has_many :comments, foreign_key: :author_id, dependent: :destroy
+
+end

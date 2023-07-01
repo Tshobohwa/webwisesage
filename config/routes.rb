9fa38939b839e38df13b20efee0d7a0b76c33676
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-01 23:34:43 +0200

    Create post routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   root to: "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
+  get "users/:author_id/posts/new", to: "posts#new"
+  post "users/:author_id/posts", to: "posts#create"
   get "/users/:author_id/posts/:id", to: "posts#show"
+  get "users/:author_id/posts/:id/comments/new", to: "comments#new"
+  post "users/:author_id/posts/:id/comments", to: "comments#create"
+  post "users/:author_id/posts/:id", to: "likes#create", as: "like_post"
 
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :new]
 end

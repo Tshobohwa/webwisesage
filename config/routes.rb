f222332df0cf7dcaba62901858ba42c5bc180b43
cherelemma <chere.lemma@aastu.edu.et>
2023-07-14 21:56:04 +0300

    add devise_for :user cto the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root to: "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"

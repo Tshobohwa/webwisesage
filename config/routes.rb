4506b390b46e631daf04f87364b6a93d5c7dbe08
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-14 14:32:45 +0200

    Name routes and create test db

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: "users#index"
-  get "/users/:id", to: "users#show"
-  get "/users/:id/posts", to: "posts#index"
+  root to: "users#index", as: "users"
+  get "/users/:id", to: "users#show", as: "user"
+  get "/users/:id/posts", to: "posts#index", as: "posts"
   get "users/:author_id/posts/new", to: "posts#new"
   post "users/:author_id/posts", to: "posts#create"
-  get "/users/:author_id/posts/:id", to: "posts#show"
+  get "/users/:author_id/posts/:id", to: "posts#show", as: "post"
   get "users/:author_id/posts/:id/comments/new", to: "comments#new"
   post "users/:author_id/posts/:id/comments", to: "comments#create"
   post "users/:author_id/posts/:id", to: "likes#create", as: "like_post"

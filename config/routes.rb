8229c1c5811c062a23dc76134b1d4eb072a50a99
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-07-15 10:15:32 +0200

    Create endpoints for post

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get "users/:author_id/posts/:id/comments/new", to: "comments#new"
   post "users/:author_id/posts/:id/comments", to: "comments#create"
   post "users/:author_id/posts/:id", to: "likes#create", as: "like_post"
+  get "api/users/:author_id/posts", to: "api/posts#index"
 
   resources :posts, only: [:index, :show, :new]
 end
